5ef9eb275300710ecccebba98e059d385341eff2
mbsantiago <santiago.mbal@gmail.com>
2020-04-26 16:31:44 -0500

    fixed migration dependencies

--- irekua_models/migrations/0001_initial.py
+++ irekua_models/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('irekua_database', '0003_auto_20200426_1412'),
+        ('irekua_database', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
